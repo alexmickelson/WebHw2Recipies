1ed78f862234b9932e00ee0c268737274fddd85b
alexmickelson96 <alexmickelson96@gmail.com>
2019-02-05 12:18:14 -0700

    asdf

---
+++ TodoCore/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoCore.Models
+{
+    public class Ingredient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Recipe> Recipes { get; set; }
+    }
+}
---
+++ TodoCore/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoCore.Models
+{
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
